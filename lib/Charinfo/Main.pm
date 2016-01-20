fa1435bb2cbfdccba097ef7fcb8b915e704c9007
Wakaba <wakaba@suikawiki.org>
2016-01-20 21:05:57 +0900

    Link to Unihan

--- lib/Charinfo/Main.pm
+++ lib/Charinfo/Main.pm
@@ -236,8 +236,10 @@ if (@char == 1) {
 
   pf q{<p>
     [<a href="https://wiki.suikawiki.org/n/%s">Notes</a>]
-    [<a href="http://unicode.org/cldr/utility/character.jsp?a=%04X">Unicode properties</a>]},
+    [<a href="http://unicode.org/cldr/utility/character.jsp?a=%04X">Unicode</a>]
+    [<a href="http://www.unicode.org/cgi-bin/GetUnihanData.pl?codepoint=%04X&amp;useutf8=true">Unihan</a>]},
       (ord $char[0] > 0x10FFFF ? (sprintf 'U-%08X', ord $char[0]) : (sprintf 'U%%2B%04X', ord $char[0])),
+      ord $char[0],
       ord $char[0];
 
   __PACKAGE__->ads;
